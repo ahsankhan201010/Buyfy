a7493426febd181570e1df912303967af223adca
ahsankhan201010 <ahsankhan201010@gmail.com>
2020-06-02 07:59:46 +0500

    attached routing and defined all routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Shop from './Pages/Shop/Shop';
+import Category from './Pages/Cateogry/Category';
+import Product from './Pages/Product/Product';
+import Auth from './Pages/Auth/Auth';
+import Checkout from './Pages/Checkout/Checkout';
+import Profile from './Pages/Profile/Profile';
 
 function App() {
   return (
     <div>
-      <h1>Buyfy</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
+        <Route path="/shop/category/:categoryName" exact component={Category} />
+        <Route path="/shop/category/:categoryName/product/:productId" component={Product} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/manageprofile" component={Profile} />
+      </Switch>
     </div>
   );
 }
