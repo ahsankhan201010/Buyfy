2f1ede3e32b8e52ba723204586b48d3df4c46ead
ahsankhan201010 <ahsankhan201010@gmail.com>
2020-06-08 13:52:31 +0500

    Reusable Component - Input Field

---
+++ src/Components/StyledInput/StyledInput.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./StyledInput.css";
+
+const StyledInput = (props) => {
+  var {value = "", placeholder = "label" , ...restInputProps} = props;
+  var [focused, setFocus] = useState(false);
+  console.log(value);
+  return (
+    <div className="styled-input-container flex">
+      <div className="input-container">
+        <input
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          type="text"
+          value={value}
+          {...restInputProps}
+        />
+        <span className={`${(focused || value) && "focusedLabel"}`}>{placeholder}</span>
+      </div>
+      <div className="input-outline-container flex">
+        <div className={`${(focused || value) && "input-outline-filler-focused"}`}></div>
+      </div>
+    </div>
+  );
+};
+
+export default StyledInput;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "./../../Components/Header/Header";
 import Paragrapgh from "./../../Components/Paragrapgh/Paragraph";
-import Button from './../../Components/Button/Button';
+import Button from "./../../Components/Button/Button";
+import StyledInput from "./../../Components/StyledInput/StyledInput";
 
 const Home = () => {
+  var [ email, setEmail ] = useState("");
+  console.log(email)
   return (
     <div>
       <h1>Home Page</h1>
@@ -24,7 +27,17 @@ const Home = () => {
         velit rem magni enim quis officia possimus obcaecati illum suscipit
         amet, molestiae officiis est porro vero veniam! Consequuntur, nam quod?
       </Paragrapgh>
+      <input
+        type="text"
+        value={email}
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="hello world"
+      />
       <Button>submmit</Button>
+      <div style={{ width: "600px", margin: "4rem" }}>
+        <StyledInput placeholder="enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
     </div>
   );
 };
