c1971b72da7e1f44b2d5d70e824cc70d204ad5a1
ahsankhan201010 <ahsankhan201010@gmail.com>
2020-06-12 11:03:02 +0500

    fake data added

---
+++ src/utility/data.js
@@ -0,0 +1,137 @@
+export var products = [
+  {
+    name: "cap1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/cap.jpg",
+    postedAT: "20-05-2020",
+    category: "caps",
+    cost: 500
+  },
+  {
+    name: "cap2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/cap.jpg",
+    postedAT: "02-05-2020",
+    category: "caps",
+    cost: 800
+  },
+  {
+    name: "cap3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/cap.jpg",
+    postedAT: "10-05-2020",
+    category: "caps",
+    cost: 400
+  },
+  {
+    name: "shoe1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/shoes.jpg",
+    postedAT: "10-05-2020",
+    category: "shoes",
+    cost: 2000
+  },
+  {
+    name: "shoe2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/shoes.jpg",
+    postedAT: "10-05-2020",
+    category: "shoes",
+    cost: 4500
+  },
+  {
+    name: "shoe3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/shoes.jpg",
+    postedAT: "25-05-2020",
+    category: "shoes",
+    cost: 7800
+  },
+  {
+    name: "hoodie1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/hoodie.jpg",
+    postedAT: "25-05-2020",
+    category: "hoodies",
+    cost: 3000
+  },
+  {
+    name: "hoodie2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/hoodie.jpg",
+    postedAT: "25-05-2020",
+    category: "hoodies",
+    cost: 3000
+  },
+  {
+    name: "hoodie3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/hoodie.jpg",
+    postedAT: "25-05-2020",
+    category: "hoodies",
+    cost: 2800
+  },
+  {
+    name: "glass1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/glasses.jpg",
+    postedAT: "25-05-2020",
+    category: "glasses",
+    cost: 2700
+  },
+  {
+    name: "glass2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/glasses.jpg",
+    postedAT: "25-05-2020",
+    category: "glasses",
+    cost: 3000
+  },
+  {
+    name: "glass3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/glasses.jpg",
+    postedAT: "25-05-2020",
+    category: "glasses",
+    cost: 5000
+  },
+  {
+    name: "watch1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/watch.jpg",
+    postedAT: "25-05-2020",
+    category: "watches",
+    cost: 8000
+  },
+  {
+    name: "watch2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/watch.jpg",
+    postedAT: "25-05-2020",
+    category: "watches",
+    cost: 9800
+  },
+  {
+    name: "watch3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa animi obcaecati quos laboriosam dignissimos impedit repellendus amet autem voluptate labore",
+    imageUrl: "./images/watch.jpg",
+    postedAT: "25-05-2020",
+    category: "watches",
+    cost: 12000
+  }
+];
